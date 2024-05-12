06461d3c4b8abf0245deabe788f3ea98a658ca49
Theo <theopremartin@orange.fr>
2024-05-12 22:46:14 +0200

    service is done now, next step controller with GET and POST

--- src/test/java/aiss/vimeominer/service/VimeoAPIServiceTest.java
+++ src/test/java/aiss/vimeominer/service/VimeoAPIServiceTest.java
@@ -2,12 +2,15 @@ package aiss.vimeominer.service;
 
 import org.junit.jupiter.api.Test;
 
+import aiss.vimeominer.model.channel.SimpleChannel;
+
 public class VimeoAPIServiceTest {
 
     @Test
     public void testGetChannelInfo() {
+        VimeoAPIService vimeoAPIService = new VimeoAPIService(); // Créez une instance du service
+        SimpleChannel channelInfo = vimeoAPIService.getChannelInfo("1234"); // Remplacez "YOUR_CHANNEL_ID_HERE" par l'ID de votre chaîne Vimeo
         
+        System.out.println(channelInfo); // Affichez les informations de la chaîne
     }
 }
-
-
