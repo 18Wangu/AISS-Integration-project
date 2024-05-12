076081281a275a9a660a7b7f79347022d40f2f6d
Theo <theopremartin@orange.fr>
2024-05-12 23:21:25 +0200

    GET work now I will do POST

--- src/main/java/aiss/vimeominer/controller/VimeoController.java
+++ src/main/java/aiss/vimeominer/controller/VimeoController.java
@@ -1,12 +1,17 @@
 package aiss.vimeominer.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import aiss.vimeominer.model.channel.SimpleChannel;
 import aiss.vimeominer.service.VimeoAPIService;
 
 @RestController
-@RequestMapping("/videominer/channel")
+@RequestMapping("/vimeominer/channel")
 public class VimeoController {
 
     private final VimeoAPIService vimeoAPIService;
@@ -14,6 +19,15 @@ public class VimeoController {
     public VimeoController(VimeoAPIService vimeoAPIService) {
         this.vimeoAPIService = vimeoAPIService;
     }
-    
-    
+
+    @GetMapping("/{channelId}")
+    public ResponseEntity<SimpleChannel> getChannelInfo(@PathVariable String channelId) {
+        SimpleChannel channelInfo = vimeoAPIService.getChannelInfo(channelId);
+        
+        if (channelInfo != null) {
+            return new ResponseEntity<>(channelInfo, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/test/java/aiss/vimeominer/service/VimeoAPIServiceTest.java
+++ src/test/java/aiss/vimeominer/service/VimeoAPIServiceTest.java
@@ -9,7 +9,7 @@ public class VimeoAPIServiceTest {
     @Test
     public void testGetChannelInfo() {
         VimeoAPIService vimeoAPIService = new VimeoAPIService(); // Créez une instance du service
-        SimpleChannel channelInfo = vimeoAPIService.getChannelInfo("1234"); // Remplacez "YOUR_CHANNEL_ID_HERE" par l'ID de votre chaîne Vimeo
+        SimpleChannel channelInfo = vimeoAPIService.getChannelInfo("1234"); 
         
         System.out.println(channelInfo); // Affichez les informations de la chaîne
     }
